01c5e999ecc19813bc0fbd89904a950c5e0a3e94
Jhonatan Henkel <jhowrf@gmail.com>
2023-09-03 21:09:05 -0300

    Criando base para o client.go

--- Client/client.go
+++ Client/client.go
@@ -1,1 +1,41 @@
-package client
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+type ExchangeRate struct {
+	USD float64 `json:"bid"`
+}
+
+func main() {
+	exchangeRate, err := requestDollarExchangeRate()
+	if err != nil {
+		panic(err)
+	}
+	println(exchangeRate.USD)
+}
+
+func requestDollarExchangeRate() (*ExchangeRate, error) {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 300*time.Microsecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		panic(err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	var exchangeRate ExchangeRate
+	err = json.NewDecoder(res.Body).Decode(&exchangeRate)
+	if err != nil {
+		return nil, err
+	}
+	return &exchangeRate, nil
+}
