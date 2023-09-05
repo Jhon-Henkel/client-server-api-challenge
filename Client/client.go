60dd550d15f1e9cbe2b6298e2a581b3bcce8938b
Jhonatan Henkel <jhowrf@gmail.com>
2023-09-05 20:09:25 -0300

    Fazendo o client salvar o retorno da cotação em um arquivo txt

--- Client/client.go
+++ Client/client.go
@@ -3,33 +3,37 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 type ExchangeRate struct {
-	USD float64 `json:"bid"`
+	USD string `json:"bid"`
 }
 
 func main() {
 	exchangeRate, err := requestDollarExchangeRate()
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		return
 	}
 	println(exchangeRate.USD)
+	saveResult(exchangeRate.USD)
 }
 
 func requestDollarExchangeRate() (*ExchangeRate, error) {
 	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, 3000*time.Microsecond)
+	ctx, cancel := context.WithTimeout(ctx, 3000*time.Millisecond)
 	defer cancel()
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8080/cotacao", nil)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer res.Body.Close()
 	var exchangeRate ExchangeRate
@@ -38,4 +42,14 @@ func requestDollarExchangeRate() (*ExchangeRate, error) {
 		return nil, err
 	}
 	return &exchangeRate, nil
+}
+
+func saveResult(result string) {
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	file.WriteString("Dólar: " + result + "\n")
+	file.Close()
 }
